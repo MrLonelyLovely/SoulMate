efa82c1c7f4eb3d43fbac0a29b545e4f55aaeff3
MrLonelyLovely <cp812034283@qq.com>
2020-03-10 19:59:19 +0800

    Initial Commit

---
+++ SoulMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoulMate
+//
+//  Created by Apui on 2020/3/10.
+//  Copyright © 2020 陈沛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
